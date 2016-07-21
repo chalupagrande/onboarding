0caa7fe1862e5da80c6e9fa6aceb47e863a74399
Jamie Skinner <jamie@ninthmind.com>
2016-07-21 15:28:43 -0500

    recalculated number of dots for page

--- dist/js/components.js
+++ dist/js/components.js
@@ -278,7 +278,13 @@ var Guide = function () {
     this.name = this._element.getAttribute('data-guide-name');
     this._tag = new GuideTag(getNode('.guide-tag[data-guide-name="' + this.name + '"]'), null, tagSpy);
 
-    this._dots = new Dots(this._element.querySelector('.progress-dots'), pages.length);
+    //check to see if guide has '.intro' page
+    var pagesWOIntro = makeArray(pages).filter(function (el) {
+      return !hasClass(el.querySelector('.page'), 'intro');
+    });
+    debugger;
+
+    this._dots = new Dots(this._element.querySelector('.progress-dots'), pagesWOIntro.length);
     this._pages = [];
     this._intro;
     this._footer = new GuideFooter(this._element.querySelector('.guide-footer'), {}, footerSpy);
